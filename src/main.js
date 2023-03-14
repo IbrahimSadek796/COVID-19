f60d387e61e7d8415d26812f8e1b31199791becf
IbrahimSadek796 <118129618+IbrahimSadek796@users.noreply.github.com>
2023-03-14 21:28:42 +0200

    done new

--- src/main.js
+++ src/main.js
@@ -7,6 +7,7 @@ import 'bootstrap/dist/css/bootstrap.min.css'
 import 'bootstrap-icons/font/bootstrap-icons.css'
 import 'bootstrap/dist/css/bootstrap.css'
 import './assets/main.css'
+import './assets/main.js'
 
 const app = createApp(App)
 
